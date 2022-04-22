7781789c0c4ad2be4bb64aaac4ebca15ff8b66b4
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-04-22 16:23:55 +0300

    fix: what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
-  return (date.getMonth() >= 2 && date.getMonth() <= 4)?'spring':(date.getMonth() >= 5 && date.getMonth() <= 7)?'summer':(date.getMonth() >= 8 && date.getMonth() <= 10)?'autumn':'winter';
+ function getSeason(date) {
+  if (arguments.length === 0) return 'Unable to determine the time of year!';
+  try {
+    date.getTime();
+  }
+  catch (e) {
+    throw new Error("Invalid date!");
+  }
+  let month = date.getMonth();
+  return (month >= 2 && month <= 4) ? 'spring' : (month >= 5 && month <= 7) ? 'summer' : (month >= 8 && month <= 10) ? 'autumn' : 'winter';
 }
 
 module.exports = {
